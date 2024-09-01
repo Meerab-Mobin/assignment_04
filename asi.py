25c808de3e01fa145bbdd4700d05dc40dc9c890c
Meerab-Mobin <mobinmeerab@gmail.com>
2024-09-01 05:02:42 -0700

    jjj

---
+++ asi.py
@@ -0,0 +1,9 @@
+# cheking conditional statements no is odd ,even
+
+num:int = int(input("enter the number: "))
+
+if num % 2 == 0 : 
+    print("the number is even")
+
+else: 
+    print("the number is odd")
