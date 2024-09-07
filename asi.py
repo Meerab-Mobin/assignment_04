9bfbc72ba0fb86a6bef7322eed35e07fa17df892
Meerab-Mobin <mobinmeerab@gmail.com>
2024-09-07 00:43:50 -0700

    jj

--- asi.py
+++ asi.py
@@ -4,17 +4,20 @@ num:int = int(input("enter the number: "))
 
 if num % 2 == 0 : 
     print("the number is even")
-
 else: 
     print("the number is odd")
 
-# to check the no. is posiive,negative or zero  
-num:int = int(input("enter the number :  "))
 
-if num =+
+# to check the no. is posiive,negative or zero  
+
+
+
+
+
+if num % +2 == 0 :
     print("the number is positive ")
 
-elif 
+elif : 
     print("the number is zero")
-else 
+else :
     print("the number is negative ")
