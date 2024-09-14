0e05ec00a260888a7263916690e2ffc5ad40ff9d
Meerab-Mobin <mobinmeerab@gmail.com>
2024-09-14 02:52:44 -0700

    dd

--- dv.py
+++ dv.py
@@ -9,3 +9,23 @@ def insert_value(arr, index, value):
     arr.insert(index, value)
     return arr
 
+
+# 2. Shopping cart program using array
+shopping_cart = []
+
+def add_item(item):
+    shopping_cart.append(item)
+    return shopping_cart
+
+def remove_item(item):
+    if item in shopping_cart:
+        shopping_cart.remove(item)
+    return shopping_cart
+
+def update_item_quantity(item, quantity):
+    # This assumes items are stored as tuples (item, quantity) in the cart
+    for i in range(len(shopping_cart)):
+        if shopping_cart[i][0] == item:
+            shopping_cart[i] = (item, quantity)
+            break
+    return shopping_cart
