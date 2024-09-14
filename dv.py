1063973a224b21ed8afe05a213fd07e5d744c6cb
Meerab-Mobin <mobinmeerab@gmail.com>
2024-09-14 02:31:40 -0700

    sj

---
+++ dv.py
@@ -0,0 +1,3 @@
+#*Create a function* that takes an array, an index, and
+#  a value as parameters. Inside the function, 
+# use the insert method to insert the value at the specified index in the array. Return the modified array.
